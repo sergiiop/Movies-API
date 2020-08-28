1b916d0dcb2450e9936db0b516936434e3f9af28
Sergio Perez <sergio-luis98@hotmail.com>
2020-08-27 23:48:24 -0500

    agregando basic y jwt

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const app = express();
-
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js'); // Exportamos nuestro archivo de rutas
+const userMoviesApi = require('./routes/userMovies')
 
+const app = express();
 // exportamos los middlewares de errores
 const {
   logErrors,
@@ -12,12 +12,12 @@ const {
 } = require('./utils/middleware/errorHandler');
 
 const notFoundHandler = require('./utils/middleware/notFoundHandler');
-
 //Body parser
 app.use(express.json()); // con este middleware permite a nuestras rutas que cuando lleven datos formato json sepa como interpretarlos
 
 //Y ejecutamos nuestra funcion de las rutas pasandole nuestra app de express
 moviesApi(app);
+userMoviesApi(app);
 
 //catch 404
 app.use(notFoundHandler);
@@ -29,5 +29,6 @@ app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, function () {
+  // eslint-disable-next-line no-console
   console.log(`Listening http://localhost:${config.port}`);
 });
